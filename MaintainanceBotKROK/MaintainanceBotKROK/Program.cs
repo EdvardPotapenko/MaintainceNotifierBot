098e00cf4f108d6771835f8ae1ca27e8344c627e
EdvardPotapenko <nothingselfless@gmail.com>
2022-04-08 10:59:57 +0300

    feat: configure as windows service

--- MaintainanceBotKROK/MaintainanceBotKROK/Program.cs
+++ MaintainanceBotKROK/MaintainanceBotKROK/Program.cs
@@ -2,27 +2,35 @@
 using Telegram.Bot;
 using Telegram.Bot.Extensions.Polling;
 
-var builder = WebApplication.CreateBuilder();
 
-string env = builder.Environment.EnvironmentName;
-builder.Configuration
-	.AddJsonFile("botsettings.json", false, false)
-	.AddJsonFile($"botsettings.{env}.json", true, false)
-	.AddEnvironmentVariables();
+var host = new HostBuilder()
+           .ConfigureHostConfiguration(builder =>
+           {
+               builder.AddEnvironmentVariables("ASPNETCORE_");
+           })
+           .ConfigureAppConfiguration((context, builder) =>
+           {
+               var env = context.HostingEnvironment.EnvironmentName;
+               builder.AddJsonFile("botsettings.json", false, false)
+                      .AddJsonFile($"botsettings.{env}.json", true, false);             
+           })
+           .UseWindowsService()
+           .ConfigureServices((hostContext, services) =>
+           {
+               if (hostContext.Configuration["BOT_TOKEN"]?.Contains("PLACEHOLDER", StringComparison.OrdinalIgnoreCase) ?? true)
+                   throw new InvalidOperationException("Bot token is null");
 
-if (builder.Configuration["BOT_TOKEN"]?.Contains("PLACEHOLDER", StringComparison.OrdinalIgnoreCase) ?? true)
-	throw new InvalidOperationException("Bot token is null");
+               if (hostContext.Configuration["BOT_MESSAGE"]?.Contains("PLACEHOLDER", StringComparison.OrdinalIgnoreCase) ?? true)
+                   throw new InvalidOperationException("Message is null or empty");
 
-if (builder.Configuration["BOT_MESSAGE"]?.Contains("PLACEHOLDER", StringComparison.OrdinalIgnoreCase) ?? true)
-	throw new InvalidOperationException("Message token is null or empty");
+               var botclient = new TelegramBotClient(hostContext.Configuration["BOT_TOKEN"]);
+               var message = hostContext.Configuration["BOT_MESSAGE"];
 
-var botclient = new TelegramBotClient(builder.Configuration["BOT_TOKEN"]);
-var message = builder.Configuration["BOT_MESSAGE"];
+               services.AddSingleton<ITelegramBotClient>(botclient);
+               services.AddSingleton<IUpdateHandler>(new BotHandler(botclient, message));
+               services.AddHostedService<BotHandlerService>();
+           });
 
-builder.Services.AddSingleton<ITelegramBotClient>(botclient);
-builder.Services.AddSingleton<IUpdateHandler>(new BotHandler(botclient, message));
-builder.Services.AddHostedService<BotHandlerService>();
-
-var app = builder.Build();
+var app = host.Build();
 
 await app.RunAsync();
